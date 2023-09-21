b93563e569eae12b15e978551fb9417a34137591
Yuri0517 <144981008+Yuri0517@users.noreply.github.com>
2023-09-21 04:36:56 +0000

    ha

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import random
 st.title('おみくじアプリ')
 
 if st.button("おみくじを引く"):
-    result=["大吉","中吉","小吉","吉","凶","大凶"]
+    results=["大吉","中吉","小吉","吉","凶","大凶"]
     result=ramdom.choice(results)
     st.write(f"結果：{result}")
 
